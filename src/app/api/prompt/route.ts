a342352f16d8af926d3167957c6c824920a62bff
Alexandru <alexdnc22@gmail.com>
2024-05-30 09:51:52 +0300

    openai key

--- src/app/api/prompt/route.ts
+++ src/app/api/prompt/route.ts
@@ -1,10 +1,10 @@
-import OpenAI from "openai";
 import { NextResponse } from "next/server";
+import OpenAI from "openai";
 
 export const runtime = "edge";
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY!,
+  apiKey: process.env.OPENAI_API_KEY as string,
 });
 
 export async function POST(req: Request) {
