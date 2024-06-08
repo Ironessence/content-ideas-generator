e8df45a5e0374daf1646065d1c8918fac5ae7648
Alexandru <alexdnc22@gmail.com>
2024-06-08 17:39:10 +0300

    add suspense to use searchParams

--- src/app/generate/layout.tsx
+++ src/app/generate/layout.tsx
@@ -1,3 +1,4 @@
+import { SkeletonCard } from "@/components/shared/SkeletonCard";
 import SkeletonPage from "@/components/shared/SkeletonPage";
 import TypeOfContentSelect from "@/components/shared/TypeOfContentSelect";
 import { Suspense } from "react";
@@ -9,7 +10,9 @@ export default function GenerateLayout({
 }>) {
   return (
     <div className="flex flex-col items-center gap-3">
-      <TypeOfContentSelect />
+      <Suspense fallback={<SkeletonCard />}>
+        <TypeOfContentSelect />
+      </Suspense>
       <Suspense fallback={<SkeletonPage />}>{children}</Suspense>
     </div>
   );
